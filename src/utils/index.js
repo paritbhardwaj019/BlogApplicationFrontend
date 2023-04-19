c68b99d722b87d0d5b768d42d685ea4824d746c9
Parit Bhardwaj ⚡ <111231393+paritbhardwaj019@users.noreply.github.com>
2023-04-19 20:35:35 +0530

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
 const PRODUCTION_URL =
   "https://backendblogapplication-production.up.railway.app";
 
-export const BASE_URL = "https://backendblogapplication-production.up.railway.app/";
+export const BASE_URL = "https://backendblogapplication-production.up.railway.app";
